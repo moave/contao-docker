088587a71439fb92e845e2857d0623960cb525b2
Robert Eichholtz <info@eichie.de>
2017-06-06 02:50:07 +0200

    update test files

--- docker/php/contao_install.sh
+++ docker/php/contao_install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# download contao project while entry point is empty
 if [ `ls -1A /app | wc -l` -eq 0 ]; then
     curl -L https://github.com/contao/standard-edition/archive/${CONTAO_VERSION}.tar.gz | tar -xzp --no-same-owner -C /app --strip-components=1
-    envsubst '\$MYSQL_DATABASE \$MYSQL_USER \$MYSQL_PASSWORD \$MYSQL_HOST \$MYSQL_PORT' < /opt/parameters.yml.template > /app/app/config/parameters.yml
-    composer install -d /app
-    chown -R www-data:www-data /app
 fi
+
+# generate automatically the parameters.yml if does not exists
+if [ ! -f /app/app/config/parameters.yml ]; then
+    envsubst '\$MYSQL_DATABASE \$MYSQL_USER \$MYSQL_PASSWORD \$MYSQL_HOST \$MYSQL_PORT' < /opt/parameters.yml.template > /app/app/config/parameters.yml
+fi
+
+# composer install if vendor folder does not exists
+if [ ! -d /app/vendor ]; then
+    composer install -d /app
+fi
+
+#chown -R www-data:www-data /app
+
